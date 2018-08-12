2b9f1927427ac6b29fbc14a55c264413bed3c180
kamaroon <kamaroon@DESKTOP-B5ELPBM>
2018-08-12 12:42:13 +0530

    added test data

--- src/main/java/runner/RunCheck.java
+++ src/main/java/runner/RunCheck.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src//main//java//feature//login1.feature",glue = {"pages","teststep","runner"},tags="@smoke",monochrome=true)
+@CucumberOptions(features="src/main/java/feature/login1.feature",glue = {"pages","teststep","runner"},tags="@smoke",monochrome=true)
 public class RunCheck {
 
 }
